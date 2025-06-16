bbf64d58a51e2e4655035ceaff500a35f22d9953
Venkata Mahesh Reddy Maram <144380824+maheshreddymaram2@users.noreply.github.com>
2025-06-17 01:02:28 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,7 @@
 import './Footer.css';
-import { useEffect, useState } from 'react';
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram, FaEye } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 function Footer() {
-  const [visits, setVisits] = useState(0);
-
-  useEffect(() => {
-    // Call CountAPI to increment and get the updated value
-    fetch('https://api.countapi.xyz/hit/maheshreddy-portfolio/visits')
-      .then(res => res.json())
-      .then(data => setVisits(data.value))
-      .catch(err => console.error("Visitor count error:", err));
-  }, []);
-
   return (
     <footer className="footer">
       <div className="footer-container">
@@ -21,11 +10,6 @@ function Footer() {
         </p>
 
         <div className="footer-right">
-          <div className="visitor-counter">
-            <FaEye className="visitor-icon" />
-            <span>Visitors: {visits}</span>
-          </div>
-
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/venkata-mahesh-reddy-maram-28039626b/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
             <a href="https://github.com/maheshreddymaram2" target="_blank" rel="noreferrer"><FaGithub /></a>
